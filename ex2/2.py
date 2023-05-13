1c89dffef639da84f8c866aaf8347c54174b1936
Hwng <133522996+Hwng1209@users.noreply.github.com>
2023-05-14 01:43:30 +0700

    2.py

--- ex2/2.py
+++ ex2/2.py
@@ -1,6 +1,14 @@
+
+"""
+# Ex2: Write a NumPy program to test whether each element of a 1-D array is also present in a second array
+# Input Array1: [ 0 10 20 40 60]
+#       Array2: [10, 30, 40]
+"""
+
+
 import numpy as np
 
 Array1 = np.array([0, 10, 20, 40, 60])
-Array2 = np.array([0, 10, 20, 40, 60])
+Array2 = np.array([10, 30, 40])
 
 print(np.isin(Array1,Array2))
